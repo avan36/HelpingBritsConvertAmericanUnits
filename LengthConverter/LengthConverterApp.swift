0a7819171aa2c0e49c511233c703bdef2ed6aeba
avan36 <158532304+avan36@users.noreply.github.com>
2024-05-31 22:02:03 +0100

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Ambrose V on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
