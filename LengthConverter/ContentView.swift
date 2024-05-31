2aff4d664daf6b194962498d7938f893b99de660
avan36 <158532304+avan36@users.noreply.github.com>
2024-05-31 22:46:29 +0100

    Update ContentView.swift

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var numberAmount = 0.0
+    @State private var startingUnit = "Inches"
+    @State private var amountEntered: Bool = false
+    @State private var finalUnit = "Centimeters"
+    @FocusState private var amountIsFocused: Bool
+    let american_units = ["Inches", "Feet", "Yards", "Miles"]
+    let english_units = ["Centimeters", "Meters", "Kilometers"]
+    var converter: Double {
+        var amount = 0.0
+        if startingUnit == "Inches" {
+            amount = numberAmount * 2.54
         }
-        .padding()
+        else if startingUnit == "Feet" {
+            amount = numberAmount * 30.48
+        }
+        else if startingUnit == "Yards" {
+            amount = numberAmount * 91.44
+        }
+        else if startingUnit == "Miles" {
+            amount = numberAmount * 160934
+        }
+        
+        if finalUnit == "Centimeters" {
+            //No change
+        }
+        else if finalUnit == "Meters" {
+            amount = amount / 100
+        }
+        else if finalUnit == "Kilometers" {
+            amount = amount / 1000
+        }
+        
+        return amount
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form {
+                Section("Starting Unit") {
+                    Picker("Starting Unit", selection: $startingUnit) {
+                        ForEach(american_units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section("Final Conversion Unit") {
+                    Picker("Final Unit", selection: $finalUnit) {
+                        ForEach(english_units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section("Enter here amount to convert") {
+                    TextField("Amount", value: $numberAmount, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                        .onChange(of: numberAmount) { newValue in
+                            amountEntered = (newValue != 0)
+                                                }
+                }
+                Text("Result")
+                Text(converter, format: .number)
+                
+            }
+            .navigationTitle("Converter for Brits")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
     }
 }
 
